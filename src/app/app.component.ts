5900b5f47ef7d8121ea3f8b01bf7303ed63ac81f
fr3d0x <alfredoescalante89@gmail.com>
2018-07-20 02:32:24 -0500

    finished regular functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,32 +11,67 @@ export class AppComponent implements OnInit{
 
   public date = moment();
 
-  public days;
+  public months;
 
   public wdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
   constructor() {}
 
   ngOnInit() {
-    this.days = this.setMonthDates(this.date);
-    console.log(this.days);
   }
 
-  public setMonthDates(date) {
-    const firstDay = moment(date).startOf('M');
-    const days = Array.apply(null, { length: date.daysInMonth() })
-      .map(Number.call, Number)
-      .map(n => {
-        return moment(firstDay).add(n, 'd');
-      });
+  public generateCalendar(stringDate, numberOfDays) {
+    const date = moment(stringDate.value, 'l');
+    this.setDates(date, numberOfDays.value);
+  }
 
-    for (let n = 0; n < firstDay.weekday(); n++) {
-      days.unshift(null);
+  public setDates(date, numberOfDays) {
+    const firstDay = moment(date);
+    let currentMonth = {
+      name: this.getMonth(firstDay),
+      days: []
+    };
+    const months = [];
+    for (let n = 0; n < numberOfDays; n++) {
+      const newDate = moment(firstDay).add(n, 'd');
+      const newMonthName = this.getMonth(newDate);
+      if (currentMonth.name !== newMonthName) {
+        months.push(currentMonth);
+        currentMonth = {
+          name: newMonthName,
+          days: []
+        };
+      }
+      currentMonth.days.push(newDate);
+      if (n === numberOfDays - 1) {
+        if (!this.isInMonths(newMonthName, months)) {
+          months.push(currentMonth);
+        }
+      }
     }
-    return days;
+
+    months.forEach(month => {
+      const firstWeekDay = month.days[0].weekday();
+      for (let n = 0; n < firstWeekDay; n++) {
+        month.days.unshift(null);
+      }
+    });
+    this.months = months;
   }
 
   public isWeekend(day) {
     return day === 0 || day === 6;
+  }
+
+  private getMonth(date) {
+    return date.format('MMMM') + ' ' + date.format('YYYY');
+  }
+
+  private isInMonths(monthName, months) {
+    months.forEach(month => {
+      if (monthName === month.name) {
+        return true;
+      }
+    });
   }
 }
