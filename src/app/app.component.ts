3bfacc61b6a47ce6cee37cb185edb85b1bdf69f2
fr3d0x <alfredoescalante89@gmail.com>
2018-07-20 00:38:44 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  public date = moment();
+
+  public days;
+
+  public wdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.days = this.setMonthDates(this.date);
+    console.log(this.days);
+  }
+
+  public setMonthDates(date) {
+    const firstDay = moment(date).startOf('M');
+    const days = Array.apply(null, { length: date.daysInMonth() })
+      .map(Number.call, Number)
+      .map(n => {
+        return moment(firstDay).add(n, 'd');
+      });
+
+    for (let n = 0; n < firstDay.weekday(); n++) {
+      days.unshift(null);
+    }
+    return days;
+  }
+
+  public isWeekend(day) {
+    return day === 0 || day === 6;
+  }
 }
